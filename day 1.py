122846ca4e05da1ba230eabec37482d74f721782
lavanya241 <149890698+lavanya241@users.noreply.github.com>
2024-12-23 22:41:30 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,16 @@
+
+# BRAND NAME GENERATOR
+print("Hello This is Band Name Generator Project just a couple of questions and You will get your Band Name ")
+city = input("What is the name of city you have grew up in?")
+pet = input("What is your pet Name?")
+print ("your Band Name could be "+city +" " + pet)
+
+# MY VERSION 1.0 OF BRAND NAME GENERATOR :
+
+print("Hello This is Band Name Generator Project just a couple of questions and You will get your Band Name ")
+
+word = input("Discribe in single word the impact you want to create with your brand?")
+
+field = input("Discribe the field you want this to happen in single word")
+
+print ("your Band Name could be "+ word +" " + field)
---
+++ task.py
@@ -0,0 +1,4 @@
+print("Hello This is Band Name Generator Project just a couple of questions and You will get your Band Name ")
+city = input("What is the name of city you have grew up in?")
+pet = input("What is your pet Name?")
+print ("your Band Name could be "+city +" " + pet)
